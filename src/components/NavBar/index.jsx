f0f767ca449a4cf6e9da78643d3b944b393f5d45
Jeanne Boutry <jeanne.boutry@hotmail.fr>
2022-12-21 18:05:16 +0100

    Add navBar and verticalBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./style.module.css";
+import Logo from "../../assets/logo.png";
 
 function NavBar() {
-	return <div>navbar</div>;
+	return (
+		<div className={styles.navbar}>
+			<div>
+				<img className={styles.logo} src={Logo} alt="Logo SportSee" />
+			</div>
+			<div>Accueil</div>
+			<div>Profil</div>
+			<div>Réglage</div>
+			<div>Communauté</div>
+		</div>
+	);
 }
 
 export default NavBar;
--- src/components/VerticalBar/index.jsx
+++ src/components/VerticalBar/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./style.module.css";
+import yoga from "../../assets/yoga.png";
+import strength from "../../assets/strength.png";
+import swim from "../../assets/swim.png";
+import bike from "../../assets/yoga.png";
 
 function VerticalBar() {
-	return <div>VerticalBar</div>;
+	return (
+		<div className={styles.verticalbar}>
+			<div className={styles.icons}>
+				<img className={styles.icon} src={yoga} alt="yoga icon" />
+				<img className={styles.icon} src={swim} alt="swimming icon" />
+				<img className={styles.icon} src={bike} alt="biking icon" />
+				<img className={styles.icon} src={strength} alt="alter icon" />
+			</div>
+			<div className={styles.copyright}>Copyright, SportSee 2020</div>
+		</div>
+	);
 }
 
 export default VerticalBar;
